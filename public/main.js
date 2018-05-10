e5b00215c87eb4926c2d5e6b3738d0e16325d1f0
britney.diboll <britney.diboll@gmail.com>
2018-05-10 14:12:34 -0400

    rest button up, starting css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+function createMinefield() {
+  var minefield = {};
+  minefield.rows = [];
+  
+  for(var i = 0; i < 9; i++) {
+      var row = {};
+      row.spots = [];
+      
+      for(var j = 0; j < 9; j++) {
+          var spot = {};
+          spot.isCovered = true;
+          row.spots.push(spot);
+      }
+      
+      minefield.rows.push(row);
+  }
+  
+  return minefield;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("main", [])
+  .controller("mainController", ($scope) => {
+    $scope.minefield = createMinefield();
+  }
+)
+
+const reset = () => {
+  location.reload(forcedReload = true);
+}
