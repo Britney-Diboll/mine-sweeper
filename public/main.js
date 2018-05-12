dffec67fd652292a401f4788e84a67a6ec105c42
britney.diboll <britney.diboll@gmail.com>
2018-05-12 13:16:09 -0400

    mines randomly placed...finally

--- public/main.js
+++ public/main.js
@@ -9,13 +9,32 @@ function createGrid() {
       for(var j = 0; j < 9; j++) {
           var square = {};
           square.covered = true;
+          square.bomb = false;
           row.squares.push(square);
       }
       
       minefield.rows.push(row);
-  }
+  }  
+  placeAllMines(minefield); 
   
-  return minefield;
+  return minefield;  
+}
+
+const  chooseSquare = (minefield, row, column) => {
+  return minefield.rows[row].squares[column];
+}
+
+const placeMines = (minefield) => {
+  var row = Math.round(Math.random() * 8);
+  var column = Math.round(Math.random() * 8);
+  var square = chooseSquare(minefield, row, column);
+  square.bomb = true;
+}
+
+const  placeAllMines = (minefield) => {
+  for(var i = 0; i < 10; i++) {
+      placeMines(minefield);
+  }
 }
 
 angular
