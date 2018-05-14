4da4f9187793706bd31592b569a45f2578c066c8
britney.diboll <britney.diboll@gmail.com>
2018-05-14 07:40:34 -0400

    NOT working

--- public/main.js
+++ public/main.js
@@ -9,13 +9,14 @@ function createGrid() {
       for(var j = 0; j < 9; j++) {
           var square = {};
           square.covered = true;
-          square.bomb = false;
+          square.bomb = "empty";
           row.squares.push(square);
       }
       
       minefield.rows.push(row);
   }  
   placeMines(minefield); 
+  calculateAllNumbers(minefield);
   return minefield;  
 }
 
@@ -28,10 +29,102 @@ const placeMines = (minefield) => {
   var row = Math.round(Math.random() * 8);
   var column = Math.round(Math.random() * 8);
   var square = chooseSquare(minefield, row, column);
-  square.bomb = true;
+  square.bomb = "mine";
   }
 }
 
+function calculateNumber(minefield, row, column) {
+  var thisSquare = chooseSquare(minefield, row, column);
+  
+  // if this spot contains a mine then we can't place a number here
+  if(thisSquare.bomb == "mine") {
+      return;
+  }
+  var mineCount = 0;
+
+  // check row above if this is not the first row
+  if(row > 0) {
+      // check column to the left if this is not the first column
+      if(column > 0) {
+          // get the spot above and to the left
+          var square = chooseSquare(minefield, row - 1, column - 1);
+          if(square.bomb == "mine") {
+              mineCount++;
+          }
+      }
+
+      // get the spot right above
+      var square = chooseSquare(minefield, row - 1, column);
+      if(square.bomb == "mine") {
+          mineCount++;
+      }
+
+      // check column to the right if this is not the last column
+      if(column < 8) {
+          // get the spot above and to the right
+          var square = chooseSquare(minefield, row - 1, column + 1);
+          if(square.bomb == "mine") {
+              mineCount++;
+          }
+      }
+  }
+  // check column to the left if this is not the first column
+  if(column > 0) {
+    // get the spot to the left
+    var square = chooseSquare(minefield, row, column - 1);
+    if(square.bomb == "mine") {
+        mineCount++;
+    }
+}
+
+// check column to the right if this is not the last column
+if(column < 8) {
+    // get the spot to the right
+    var square = chooseSquare(minefield, row, column + 1);
+    if(square.bomb == "mine") {
+        mineCount++;
+    }
+}
+
+    // check row below if this is not the last row
+    if(row < 8) {
+      // check column to the left if this is not the first column
+      if(column > 0) {
+          // get the spot below and to the left
+          var square = chooseSquare(minefield, row + 1, column - 1);
+          if(square.bomb == "mine") {
+              mineCount++;
+          }
+      }
+
+      // get the spot right below
+      var square = chooseSquare(minefield, row + 1, column);
+      if(square.bomb == "mine") {
+          mineCount++;
+      }
+
+      // check column to the right if this is not the last column
+      if(column < 8) {
+          // get the spot below and to the right
+          var square = chooseSquare(minefield, row + 1, column + 1);
+          if(square.bomb == "mine") {
+              mineCount++;
+          }
+      }
+  }
+  
+  if(mineCount > 0) {
+      thisSquare.bomb = mineCount;
+  }
+}
+
+function calculateAllNumbers(minefield) {
+  for(var y = 0; y < 9; y++) {
+      for(var x = 0; x < 9; x++) {
+          calculateNumber(minefield, x, y);
+      }
+  }
+}
 
 angular
   .module("main", [])
